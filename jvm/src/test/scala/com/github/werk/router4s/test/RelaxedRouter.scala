2c1da3e0124583a97087c784861a508915a102d6
werk <mfwerk@gmail.com>
2017-04-04 23:12:36 +0200

    New router syntax experiments

--- jvm/src/test/scala/com/github/werk/router4s/test/RelaxedRouter.scala
+++ jvm/src/test/scala/com/github/werk/router4s/test/RelaxedRouter.scala
@@ -2,7 +2,7 @@ package com.github.werk.router4s.test
 
 import scala.reflect.ClassTag
 
-object DslExperiment2 {
+class RelaxedRouter[Page] {
 
     trait Path0 {
         def bind[P <: Product : ClassTag](page : P) : BoundPath
@@ -20,20 +20,25 @@ object DslExperiment2 {
 
 
     trait BoundPath {
-        def extend(p : BoundPath) : BoundPath
-        def extend[O](p : BoundPathO[O]) : BoundPathO[O]
+        def extend(p: BoundPath) : BoundPath
+        def extend[O](p: BoundPathO[O]) : BoundPathO[O]
     }
+
     trait BoundPathO[O] {
-        def extend(p : BoundPath) : BoundPathO[O]
-        def extend[O2](p : BoundPathIO[O, O2]) : BoundPathO[O2]
+        def extend(p: BoundPath) : BoundPathO[O]
+        def extend[O2](p: BoundPathIO[O, O2]) : BoundPathO[O2]
+
+        def children(rs: RouterI[O]*) : Router
     }
+
     trait BoundPathIO[I, O] {
-        def extend(p : BoundPath) : BoundPathIO[I, O]
-        def extend[O2](p : BoundPathIO[O, O2]) : BoundPathIO[I, O2]
+        def extend(p: BoundPath) : BoundPathIO[I, O]
+        def extend[O2](p: BoundPathIO[O, O2]) : BoundPathIO[I, O2]
 
-        def children[O2](rs : RouterIO[O, O2]*) : RouterIO[I, O2]
+        def children(rs: RouterI[O]*) : RouterI[I]
     }
 
-    trait RouterIO[I, O]
+    trait RouterI[I]
+    trait Router
 
 }
